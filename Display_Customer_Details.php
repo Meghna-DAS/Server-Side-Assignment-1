d8c52653a7a2da7d0017d21f35699b893ced9bf0
Meghna Das <55181652+Meghna-DAS@users.noreply.github.com>
2021-05-10 12:13:04 +0530

    Display_Customer_Details.php

--- Display_Customer_Details.php
+++ Display_Customer_Details.php
@@ -5,20 +5,20 @@
 	$password = "";
 	$database = "Customer_DB";
 
-	$conn = mysqli_connect( $server, $username, $password, $database );
+	$conn = mysqli_connect( $server, $username, $password, $database ); //connection with database
 
-	if( $conn )
+	if( $conn ) //check if connection is done
 	{
-		$query = "select * from Customer_details";
+		$query = "select * from Customer_details"; //sql query to display all customer details
 
-		$data = mysqli_query($conn,$query); //connection with database
+		$data = mysqli_query($conn,$query); 
 
-		if($data)
+		if($data) //check if data is present in database
 		{
 			echo "<body>"; 
 			echo "<center>";
 			echo "<h3> Customer Details are as follows </h3>";
-			echo "<table border='2px'><tr><th>ID</th><th>Name</th><th>Password</th><th>Email</th><th>Gender</th><th>Address</th><th>Country</th></tr>";
+			echo "<table border='2px'><tr><th>ID</th><th>Name</th><th>Password</th><th>Email</th><th>Gender</th><th>Address</th><th>Country</th></tr>"; // to display output in table format
 			while( $row = mysqli_fetch_array($data) )
 			{
 				//output statement
